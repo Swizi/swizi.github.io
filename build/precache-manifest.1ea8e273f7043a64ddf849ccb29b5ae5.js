87e8ca7bacf87815af5afb139b17c1439cf31417
Denis Bosyi <denis.bosiy@gmail.com>
2020-05-05 15:33:09 +0300

    build

---
+++ build/precache-manifest.1ea8e273f7043a64ddf849ccb29b5ae5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f6811bc49581d86c0f2f3db14e15bc1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0a5f64d3e1c025d7261",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "2e59b3ed73d5b5ad9fdc",
+    "url": "/static/js/2.2c43aeee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2c43aeee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0a5f64d3e1c025d7261",
+    "url": "/static/js/main.58a9068d.chunk.js"
+  },
+  {
+    "revision": "ec8b8793836a61bf9cb9",
+    "url": "/static/js/runtime-main.11d414c5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
